3a3a414e88557afa257988668f3879a84e169b90
alexeev <danielalexeev1990@gmail.com>
2022-05-15 20:42:08 +0300

    test arrays

--- Task4/TopGrade.cs
+++ Task4/TopGrade.cs
@@ -1,10 +1,7 @@
-﻿using System.Diagnostics;
-
-namespace Task4;
-
+﻿namespace Task4;
 
 static class TopGrade
-{   
+{
     // Сортировка подсчетом
     private static IEnumerable<int> CountingSort(IEnumerable<int> inputStream, int sortFactor, int maxValue)
     {
@@ -12,9 +9,12 @@ static class TopGrade
         int[] countArray = new int[maxValue + 1];
         for (int i = 0; i < stream.Length; i++) countArray[stream[i]]++;
         int sortedArrayIndex = 0;
-        for (int i = 0; i < countArray.Length; i++) for (int j = 0; j < countArray[i]; j++) stream[sortedArrayIndex++] = i;
+        for (int i = 0; i < countArray.Length; i++)
+        for (int j = 0; j < countArray[i]; j++)
+            stream[sortedArrayIndex++] = i;
         return stream;
     }
+
     // Переводит словарь число: количество в массив
     private static int[] DictToArray(Dictionary<short, uint> counts, int arrayLen, short minValue, short maxValue)
     {
@@ -28,6 +28,7 @@ static class TopGrade
                 array[arrayIndex] = i;
                 arrayIndex++;
             }
+
             if (value > 1)
             {
                 for (short j = 0; j < value; j++)
@@ -37,6 +38,7 @@ static class TopGrade
                 }
             }
         }
+
         return array;
     }
 
@@ -50,7 +52,8 @@ static class TopGrade
             for (int i = 0; i < enumerable.Length; i++) yield return enumerable[i];
             yield break;
         }
-        short selectN = (short)stream[0];
+
+        short selectN = (short) stream[0];
         int arrayLen = 0;
         Dictionary<short, uint> counts = new Dictionary<short, uint>();
         for (int i = 0; i < stream.Length; i++)
@@ -62,7 +65,7 @@ static class TopGrade
                 arrayLen++;
                 if (i == stream.Length - 1)
                 {
-                    int[] sortedPartStream = DictToArray(counts, arrayLen, (short) (selectN-sortFactor), selectN);
+                    int[] sortedPartStream = DictToArray(counts, arrayLen, (short) (selectN - sortFactor), selectN);
                     for (int k = 0; k < sortedPartStream.Length; k++)
                     {
                         yield return sortedPartStream[k];
@@ -71,14 +74,15 @@ static class TopGrade
             }
             else
             {
-                int[] sortedPartStream = DictToArray(counts, arrayLen, (short) (selectN-sortFactor), selectN);
+                int[] sortedPartStream = DictToArray(counts, arrayLen, (short) (selectN - sortFactor), selectN);
                 for (int k = 0; k < sortedPartStream.Length; k++)
                 {
                     yield return sortedPartStream[k];
                 }
+
                 counts = new Dictionary<short, uint>();
                 arrayLen = 0;
-                selectN = (short)stream[i];
+                selectN = (short) stream[i];
                 if (counts.TryGetValue((short) stream[i], out _)) counts[(short) stream[i]]++;
                 else counts.Add((short) stream[i], 1);
                 arrayLen++;
@@ -88,5 +92,25 @@ static class TopGrade
 
     static void Main(string[] args)
     {
+        int[] array1 = {14, 9, 4, 5, 6, 3, 2, 1};
+        foreach (var num in Sort(array1,20,14))
+        {
+            Console.WriteLine(num);
+            Console.ReadKey();
+        }
+        Console.WriteLine("---------------");
+        int[] array2 = {3, 2, 4, 3, 5, 4, 7, 6, 9, 8};
+        foreach (var num in Sort(array2,1,9))
+        {
+            Console.WriteLine(num);
+            Console.ReadKey();
+        }
+        Console.WriteLine("---------------");
+        int[] array3 = {1, 2, 3, 10, 30, 29};
+        foreach (var num in Sort(array3,1,20))
+        {
+            Console.WriteLine(num);
+            Console.ReadKey();
+        }
     }
 }
